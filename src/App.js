f6a752924b8354bfb61f3a82c2f4933b6b6ff789
Traininghubshyamlalyadav1990 <ajesh.yadav122002@gmail.com>
2020-05-28 13:07:15 +0530

    shyam-v3

--- src/App.js
+++ src/App.js
@@ -20,41 +20,53 @@ class Homepage extends Component {
   <span>
   <img src="./images/leaf.png" width="100" height="100" alt="leaf-main-image" />
   </span>
-    <h2>Technical Architect</h2>
+    <h2>Woring At Nokia AS Technical Architect</h2>
     <div class="fakeimg"  id="style1">
     My goal is to apply my technical skills in Java design and implementation, 
-    
      systems configuration and troubleshooting operations to fulfill the company’
      score goals. I aim to bring success to the company by designing, 
      integrating, and updating frameworks to sustain Java applications and providing solutions to address technical issues possibly encountered in a Java environment. My years of experience broadened my knowledge on Java technologies and enhanced my abilities in spotting trends, flaws, and developing upgrades concerning these as well. With my expertise and commitment to become a productive team player,
     I am confident that I can be an asset to the company.
       
         </div>
-   
-    <h3>More Text</h3>
-    <p>Lorem ipsum dolor sit ame.</p>
-    <div class="fakeimg"  id="style">  <img src="./images/logo.png" width="100" alt="logo" /></div><br/>
-    <div class="fakeimg" id="style">Image</div><br/>
-    <div class="fakeimg"  id="style">Image</div>
+  
+    <div class="fakeimg"> 
+   Architected solutions to health care business problems involving interactive voice response and subscriber oriented health incentive programs.
+   Advised development team on design options in line with Aetna enterprise infrastructure and project life cycle.
+   Engaged as an integrated part of project team and championed project from inception to design/development.
+   Work with business to translate business needs into functional requirements and generated preliminary solutions in conjunction with business/infrastructure/development representatives.
+   Drove high level/detailed designs on architecturally significant components based on Aetna internal standards and presented and articulated proposed solutions to architecture review group.
+   Put together architectural artifacts to form a repository representing point-of-time and finalized architectural conceptual design.
+   Worked with multiple parties to resolve time/budget constraints and resource contention issues.
+   Communicated with infrastructure/fellow architects/enterprise framework group to ensure best practices and address scalability/reusability issues. </div><br/>
+   <div class="container">
+  <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+  <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+  <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+  <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+  <iframe class="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+
+</div>
+    </div>
+    <div>
+    <span class="label success">Success</span>
+   <span class="label info">Info</span>
+  <span class="label warning">Warning</span>
+  <span class="label danger">Danger</span>
+   <span class="label other">Other</span>
+
+
     </div>
 </div>
-  <div class="main">
-<h2>TITLE HEADING</h2>
-<h5>Title description, Dec 7, 2017</h5>
-<div class="fakeimg"  id="style">Image</div>
-    <p>Some text..</p>
-    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-    <br></br>
-    <h2>TITLE HEADING</h2>
-    <h5>Title description, Sep 2, 2017</h5>
-    <div class="fakeimg" id="style">Image</div>
-    <p>Some text..</p>
-    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-
-  </div>
+ 
 
 <div class="footer">
-  <h2>Footer</h2>
+  <h2>Footer
+  </h2>
+  <spa>@CopyRight RSB Infotech pvt ltd </spa>
+  <li><span>
+    <a href="portfolio.html" target="_blank">See my portfolio</a>
+</span></li>
 </div>
 
 </div>
